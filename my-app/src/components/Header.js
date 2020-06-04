6a908cad9fb94a42b04c465198ba7a6a53eda40e
danCarmody <danCarmody93@gmail.com>
2020-06-04 14:52:17 -0700

    Finished Header.js created Resume Data

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -19,11 +19,27 @@ class Header extends Component {
 
                     <ul id="nav" className="nav">
                         <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-                        <li
+                        <li><a className="smoothscroll" href="#about">About</a></li>
+                            <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                        <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+                        <li><a className="smoothscroll" href="#contact">Contact</a></li>
                     </ul>
-
-
                 </nav>
+
+                <div className="row banner">
+                    <div className="banner-text">
+                        <h1 className="responsive-headline">I'm {name}.</h1>
+                        <h3>I'm a {city} based <span>{occupation}</span>. {description}</h3>
+                        <hr />
+                        <ul className="social">
+                            {networks}
+                        </ul>
+                    </div>
+                </div>
+
+                <p className="scrolldown">
+                    <a className="smoothscroll" href="#about"><i className="icon-down-cirlce"></i></a>
+                </p>
 
             </header>
         );
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,4 +5,4 @@ import App from './App';
 import registerServiceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
