0f370763b4ebb034fd4718d9a93e2e69d73a1ca1
danCarmody <danCarmody93@gmail.com>
2020-06-09 13:13:28 -0700

    finished out the return statement, portfolio now just needs images

--- my-app/src/components/Portfolio.js
+++ my-app/src/components/Portfolio.js
@@ -3,13 +3,20 @@ import React, { Components} from 'react';
 class Portfolio extends Components{
     render() {
         if(this.props.data) {
-            var title = this.props.data.title;
-            var desc = this.props.data.description;
-            var link = this.props.data.link;
+            
         }
 
         return(
-            
+           <section id="portfolio">
+               <div className="row">
+                   <div className="twelve columns collapsed">
+                       <h1>Check Out Some Of My Work.</h1>
+                       <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                           {projects}
+                       </div>
+                   </div>
+               </div>
+           </section> 
         );
     }
 }
